c4483456aa5d42a5ec99708a97443f4645bf561f
EarlWSC <ocdaoch@wsc.school.nz>
2024-06-14 12:58:02 +1200

    Sector 1 40%

--- main.py
+++ main.py
@@ -1,26 +1,54 @@
 import random
+import time
 # Dictionary
-QFORMAT_2 = "{}\n A. {} \n B. {} \n  Decision Here: "
-QFORMAT_3 = "{}\n A. {} \n B. {} \n C. {} \n Decision Here: "
-QFORMAT_4 = "{}\n A. {} \n B. {} \n C. {} \n D. {}\n Decision Here: "
+QFORMAT_2 = "{}\nA. {}\nB. {}\nDecision Here:"
+QFORMAT_3 = "{}\nA. {}\nB. {}\nC. {}\nDecision Here:"
+QFORMAT_4 = "{}\nA. {}\nB.{}\nC. {}\nD. {}\nDecision Here:"
 
-SHIPWRECK = [["Stay on the Ship and wait to be rescued...", "Leave and Explore into the unknown and potentially deadly island..."],
-             ["Wait longer, I hope we get rescued very soon...", "Leave to find food, I'm absolutely starving..."]]
-BEACH_HUT = []
-LIGHTHOUSE = []
-SHIPWRECK_ANS =  [["Stay", "Explore"],
-              ["Wait Longer", "Look for Food"]]
-BEACH_HUT_ANS = [["Go Back", "Explore", "Scavenge"]]
-LIGHTHOUSE_ANS = [["Explore", "Stay"]]
+# Prompts/ Questions
+INTRO_PROMPTS = ["Welcome! Do you have what it takes to survive? "]
 
+PROMPTS_1 = ["You survived a plane crash, and was washed ashore on an island with an inflatable raft. You decide whether to stay in the raft or explore. Do you...",
+             "You decide to explore the island for whatever can help you temporarily. You suddenly stumble upon an array of small wooden houses. Do you...",
+             "After that, you managed to stumble upon an abandoned lighthouse after trekking for a while. Do you...",
+             "You decide to remain in the boat. Now you're starting to become hungry. Do you...",
+             "You decide to wait out for longer. It is getting dark. Do you...",
+             "You're starting to suffer from starvation. Do you...",
+             "You decide to find food. Do you...",]
+
+PROMPTS_2 = ["You decide to either continue on the forest, or the houses, or the lighthouse. Do you...",
+             "You decide to sleep since its become dark again. You then managed to wake up in the middle of the night. Do you...",
+             "Suddenly, you hear a weird noise as you see a pair of white eyes staring at you. You decide whether to run off or get a weapon. Do you...",
+             "You decide fight it. Do you aim for its head or its body?",
+             "You decide to run off. You managed to evade whatever that thing is. Do you go to the houses or the forest?",
+             "You're currently on the run. Do you plan to hide under the bushes or behind a tree?",]
+
+OUTCOMES =[""]
+
+ENDINGS =[""]
+
+Q_INTRO = [["Yes","No","Dunno"]]
+Q_SHIPWRECK = [["STAY on the Ship and wait to be rescued...", "Leave and EXPLORE into the unknown and potentially deadly island..."],
+             ["WAIT LONGER, I hope we get rescued very soon...", "Let's LOOK FOR FOOD, I'm starving..."]]
+Q_BEACH_CAVE = []
+Q_LIGHTHOUSE = []
+
+# Available Answers
+INTRO_ANS = ["yes", "no"]
+SHIPWRECK_ANS =  [["stay", "explore"],
+              ["wait longer", "look for food"]]
+BEACH_CAVE_ANS = [["go back", "explore", "scavenge"]]
+LIGHTHOUSE_ANS = [["explore", "stay"]]
 S_OPTIONS = [["a", "b"],
              ["a", "b", "c"],
              ["a", "b", "c", "d"]]
 
 # Functions
-health = 5
-def intro():
-    print(r"""  
+Health = 5
+
+# IIIIIIIIINNNNNNNNNNNNNNNNNTTTTTTTTTTTTTTTTTRRRRRRRRRRRRRROOOOOOOOOOOOOOOOOOOOOOO
+print("You are now playing,")
+print(r"""  
   ██████  █    ██  ██▀███   ██▒   █▓ ██▓ ██▒   █▓ ▄▄▄       ██▓        ▒█████    █████▒   ▄▄▄█████▓ ██░ ██ ▓█████     ██▓  ██████  ██▓    ▓█████ 
 ▒██    ▒  ██  ▓██▒▓██ ▒ ██▒▓██░   █▒▓██▒▓██░   █▒▒████▄    ▓██▒       ▒██▒  ██▒▓██   ▒    ▓  ██▒ ▓▒▓██░ ██▒▓█   ▀    ▓██▒▒██    ▒ ▓██▒    ▓█   ▀ 
 ░ ▓██▄   ▓██  ▒██░▓██ ░▄█ ▒ ▓██  █▒░▒██▒ ▓██  █▒░▒██  ▀█▄  ▒██░       ▒██░  ██▒▒████ ░    ▒ ▓██░ ▒░▒██▀▀██░▒███      ▒██▒░ ▓██▄   ▒██░    ▒███   
@@ -31,40 +59,15 @@ def intro():
 ░  ░  ░   ░░░ ░ ░   ░░   ░      ░░   ▒ ░     ░░    ░   ▒     ░ ░      ░ ░ ░ ▒   ░ ░         ░       ░  ░░ ░   ░       ▒ ░░  ░  ░    ░ ░      ░   
       ░     ░        ░           ░   ░        ░        ░  ░    ░  ░       ░ ░                       ░  ░  ░   ░  ░    ░        ░      ░  ░   ░  ░
                                 ░            ░                                                                                                   """)
-print
+confidence = input("{}".format(INTRO_PROMPTS[0])).lower()
+if confidence == INTRO_ANS[0]:
+    print("Well well... We'll see about that!")
+elif confidence == INTRO_ANS[1]:
+    print("NIG")
 
 # SHIPWRECK PHASE
-intro()
 
-print("You survived a plane crash, and was washed ashore on an island with an inflatable raft. You decide whether to stay in the raft or explore.")
-
-answer = input("Welcome, do you have what it takes to survive?")
-
-print(QFORMAT_2.format(SHIPWRECK[0][0],SHIPWRECK[0][1]))
-
-
-answer = input()
-
-if answer == SHIPWRECK_ANS[0][0]:
-    print("fanum")
-elif answer == SHIPWRECK_ANS[0][1]:
-    print("skibidi")
-
-# BEACH HUT PHASE
-
-print("You decide to explore the island for whatever can help you temporarily. You suddenly stumble upon an array of small wooden houses. Do you...")
-
-answer == input()
-
-if answer == BEACH_HUT_ANS[0][0]:
-  print("rizz")
-elif answer == BEACH_HUT[0][1]:
-  print("adin ross")
-elif answer == BEACH_HUT[0][2]:
-  print("grimace shake")
+# BEACH CAVE PHASE
 
 # LIGHTHOUSE PHASE
 
-print("After that, you managed to stumble upon an abandoned lighthouse after trekking for a while. Do you...")
-
-answer == input()
